c172cb6e965f765857af58570e042c1289189f73
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-25 04:02:22 +0300

    converted into a command line script

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
+from getopt import GetoptError, getopt
 import json
 import os
 import re
+import sys
 import pandas as pd
 
 from sv_portal_reader import SVPortalParser
 
-df = pd.read_csv("codes.csv")
 def write_list(parsed_deck: SVPortalParser, i):
     outdir = './training'
     file = f'{parsed_deck.craft}-{i}.csv'
     parsed_deck.cards_df.to_csv(os.path.join(outdir, file),index=False)
 
 
-tourney_decks = pd.read_csv("J1kNoaYPvxQx-decks.csv")
-tourney_list = tourney_decks.values.tolist()
+def main(argv):
 
-for idx, deck in enumerate(tourney_list):
-    parsed1= SVPortalParser(deck[1])
-    parsed2= SVPortalParser(deck[2])
+    data_file = ''
+    outdir = './training'
 
-    parsed1.parse_deck()
-    parsed2.parse_deck()
+    try:
+        opts, args = getopt(argv, "i:o:")
+    except GetoptError:
+        print("train.py -i <inputfile> -o <outputfile>")
+        sys.exit(2)
 
-    write_list(parsed1, idx)
-    write_list(parsed2, idx)
+    for opt, arg in opts:
+        if opt == "-i":
+            data_file = arg
+        elif opt == "-o":
+            outdir = arg
+
+    tourney_decks = pd.read_csv(data_file)
+    tourney_list = tourney_decks.values.tolist()
+
+    for idx, deck in enumerate(tourney_list):
+        parsed1= SVPortalParser(deck[1])
+        parsed2= SVPortalParser(deck[2])
+
+        parsed1.parse_deck()
+        parsed2.parse_deck()
+
+        write_list(parsed1, idx)
+        write_list(parsed2, idx)
+    
+
+if __name__=="__main__":
+    main(sys.argv[1:])
