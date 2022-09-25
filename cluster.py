762e996f848b4e565f4c8e211511465c9729f23e
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-25 03:51:49 +0300

    refacted to use new Vectorizer class

--- cluster.py
+++ cluster.py
@@ -8,37 +8,10 @@ import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+from vectorizer import ClassVectorizer
+
 NUM_CLUSTERS = 4
 NUM_TOP_VERS = 30
-
-
-f = open('cards.json')
-cards_db = json.load(f)
-
-class ClassVectorizer:
-
-    def __init__(self, craft):
-        self.craft = craft
-        self.card_pool = []
-        self.vectorized = []
-        self.decks = []
-
-        self.initialize()
-
-    def initialize(self):
-        self.card_pool = get_craft_vector(self.craft)
-
-    def vectorize(self, deck):
-        vector = [0]*len(self.card_pool)
-
-        for (idx, name) in enumerate(self.card_pool):
-            for [card, copies] in deck:
-                if card == name:
-                    vector[idx] += int(copies)
-
-        self.vectorized.append(vector)
-        self.decks.append(deck)
-
 
 
 def card_names(_deck):
@@ -53,21 +26,7 @@ def most_common_cards(_deck, k):
 def decks_by_label(a_label, _labeled):
     return [(deck, label) for (deck, label) in _labeled if label == a_label]
 
-def get_craft_vector(craft):
-    crafts = ['forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
-    
-    if craft not in crafts:
-        return
 
-    craft_name = craft + 'craft'
-    scope = (craft_name, 'netural')
-    ignored = ("promo", "token")
-    filtered = [card["name_"] for card in cards_db.values() if card["craft_"].lower() in scope and card["expansion_"].lower() not in ignored]
-
-    unique = set(filtered)
-    sorted = np.sort(list(unique))
-    
-    return sorted
 
 crafts = ['forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
 
