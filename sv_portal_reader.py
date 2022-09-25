c3b205b4ba4e37a30d8653db9074a309f40d2045
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-25 19:53:16 +0300

    now is a singleton

--- sv_portal_reader.py
+++ sv_portal_reader.py
@@ -1,56 +1,62 @@
 import json
 import pandas as pd
 
-hashes = pd.read_csv("codes.csv")
+hashes = pd.read_csv("db/codes.csv")
 
 class SVPortalParser:
 
 
-    def __init__(self, deck_link):
-        self.deck_link = deck_link
+    def __init__(self, format_data={}):
         self.deck = {}
-        self.hash_list = []
+
         self.craft = ""
-        self.cards = []
+        self.archetypes = format_data
 
-    def parse_craft(self):
+    def parse_craft(self, deck_link):
         classes = ['forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
-        class_code = int(self.deck_link.split('.')[2]) - 1
+        class_code = int(deck_link.split('.')[2]) - 1
 
-        self.craft = classes[class_code]
+        return classes[class_code]
     
-    def prase_hashes(self):
-        card_hashes = self.deck_link.split('.',3)[3:]
+    def prase_hashes(self, deck_link):
+        card_hashes = deck_link.split('.',3)[3:]
         card_hashes = ".".join(card_hashes)
         card_hashes = card_hashes.split('?')[0]
         card_hashes = card_hashes.split('.')
-        
-        self.hash_list = card_hashes
+        return card_hashes
     
-    def parse_deck(self):
-        self.parse_craft()
-        self.prase_hashes()
+    def parse_deck(self, deck_link):
+        craft = self.parse_craft(deck_link)
+        hash_list = self.prase_hashes(deck_link)
 
-        d = pd.DataFrame(self.hash_list)
+        d = pd.DataFrame(hash_list)
         d = d[0].value_counts().reset_index().rename(columns={'index': 'code', 0: 'count'})
         deck_df = pd.merge(d, hashes)[['count', 'card_name']]
         deck_df = deck_df.groupby('card_name', as_index = False).agg('sum')
 
-        self.cards = deck_df.values.tolist()
-        self.cards_df = deck_df
-    
-    def find_archetype(self):
-        f = open('./roar-of-godwyrm.json')
-        archetypes = json.load(f)
+        cards = deck_df.values.tolist()
+        cards_df = deck_df
+        archetype = self.find_archetype(cards, craft)
 
-        craft_archetypes = archetypes[self.craft]
+        deck_data = {
+            "link": deck_link,
+            "archetype": archetype,
+            "craft": craft,
+            "cards_df": cards_df
+        }
+
+        return deck_data
+    
+    def find_archetype(self, cards, craft):
+
+        craft_archetypes = self.archetypes[craft]
         current_score = 0
         current_archetype = ""
 
         for (name, details) in craft_archetypes.items():
             archetype_score = 0
             for card in details["feature_cards"]:
-                for (card_n, copies) in self.cards:
+                for (card_n, copies) in cards:
                     if card_n == card:
                         archetype_score += copies
 
@@ -59,15 +65,7 @@ class SVPortalParser:
                 current_archetype = name  
 
         
-        self.archetype = current_archetype
-
-
-    def get_deck_data(self):
-        return {
-            "link": self.deck_link,
-            "archetype": self.archetype,
-            "craft": self.craft
-        }
+        return current_archetype
 
             
 
