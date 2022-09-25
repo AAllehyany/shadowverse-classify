b1f0400210aa9f76ab866dce5a46ab7eada468cc
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-25 19:10:05 +0300

    refactored into singleton vectorizer class

--- vectorizer.py
+++ vectorizer.py
@@ -1,36 +1,42 @@
 import numpy as np
-import json
 
-f = open('cards.json')
-cards_db = json.load(f)
-def get_craft_vector(craft):
-    crafts = ['forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
-    
-    if craft not in crafts:
-        return
 
-    craft_name = craft + 'craft'
-    scope = (craft_name, 'netural')
-    ignored = ("promo", "token")
-    filtered = [card["name_"] for card in cards_db.values() if card["craft_"].lower() in scope and card["expansion_"].lower() not in ignored]
+class Vectorizer:
 
-    unique = set(filtered)
-    sorted = np.sort(list(unique))
-    
-    return sorted
+    def __init__(self, all_cards):
+        self.all_cards = all_cards
+        self.vectorizers = {}
+        self.crafts = ['forest', 'sword', 'rune', 'dragon', 'shadow', 'blood', 'haven', 'portal']
+
+    def initialize(self):
+        
+        
+        for craft in self.crafts:
+            craft_name = craft + 'craft'
+            scope = (craft_name, 'netural')
+            ignored = ("promo", "token")
+            filtered = [card["name_"] for card in self.all_cards.values() if card["craft_"].lower() in scope and card["expansion_"].lower() not in ignored]
+
+            unique = set(filtered)
+            sorted = np.sort(list(unique))
+            self.vectorizers[craft] = ClassVectorizer(craft, sorted)
+
+    def vectorize(self, deck, craft):
+        if craft not in self.crafts:
+            return
+        
+        self.vectorizers[craft].vectorize(deck)
 
 class ClassVectorizer:
 
-    def __init__(self, craft):
+    def __init__(self, craft, card_pool):
         self.craft = craft
-        self.card_pool = []
+        self.card_pool = card_pool
         self.vectorized = []
         self.decks = []
 
-        self.initialize()
-
-    def initialize(self):
-        self.card_pool = get_craft_vector(self.craft)
+    # def initialize(self):
+    #     self.card_pool = get_craft_vector(self.craft)
 
     def vectorize(self, deck):
         vector = [0]*len(self.card_pool)
